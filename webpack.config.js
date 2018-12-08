5c490b0b7f8242fa53bddb847b97272f3158b639
cosola <scvargas@cosola.org>
2018-12-07 22:57:50 -0500

    Completed part I of header

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var path = require ('path');   //Path is part of node
+
+module.export = {
+	entry: "./app/assets/scripts/App.js",
+	output: {
+		path: path.resolve( __dirname, "./app/temp/scripts" ),  //Will generate an absolute path as required by Webpack
+		filename: "App.js",     // filename could be any anme
+
+	}
+
+}
