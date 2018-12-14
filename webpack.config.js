0fad475df25e19e60d12404f0c78daedddf4c7b7
cosola <scvargas@cosola.org>
2018-12-13 21:49:43 -0500

    Completed styling side-header for small screeens

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
-//var path = require ('path');   //Path is part of node
+const path = require ('path');   //Path is part of node
 
-module.export = {
+module.exports = {
 	entry: "./app/assets/scripts/App.js",
 	output: {
 		path: path.resolve( __dirname, "./app/temp/scripts" ),  //Will generate an absolute path as required by Webpack
-		filename: "App.js",     // filename could be any anme
+		filename: "App.js"     // filename could be any anme
 
+	},
+	module: {
+		rules: [
+			{
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015']
+				},	
+				test: /\.js$/,    		//Apply babel-loader only to js files
+				exclude: /[node_modules]/   		 //exclude node_modules in conversion
+			}
+		]
 	}
 
 }
